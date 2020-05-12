4b948afeaf5f760f1a6a54616d250949483b391e
https://github.com/mishhubc <mihaijicum@gmail.com>
2020-05-12 18:25:10 +0200

    Updates

---
+++ precache-manifest.34aca0d75977ad7e3dfe5cb011794d45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232f59563f00773bf886c3c9f927bef1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ad4e314ddf2cbd0ef250",
+    "url": "/robofriends/static/css/main.2b76312f.chunk.css"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad4e314ddf2cbd0ef250",
+    "url": "/robofriends/static/js/main.71a8b02b.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  },
+  {
+    "revision": "3c5595311a8973211e9c261ffb22cd89",
+    "url": "/robofriends/static/media/VoiceActivatedBB_reg.3c559531.otf"
+  }
+]);
